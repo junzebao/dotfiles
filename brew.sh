8cdf0708e0fd27318ee83735cc59c7512b994192
Junze Bao <junzebao@gmail.com>
2026-05-04 21:58:18 +0200

    add maccy
    Signed-off-by: Junze Bao <junzebao@gmail.com>

--- brew.sh
+++ brew.sh
@@ -1,18 +1,21 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 /opt/homebrew/bin/brew shellenv zsh
-echo >> /Users/junze/.zprofile & echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> /Users/junze/.zprofile & eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+echo >>/Users/junze/.zprofile &
+echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >>/Users/junze/.zprofile &
+eval "$(/opt/homebrew/bin/brew shellenv zsh)"
 brew install neovim
 brew install zoxide
 
 brew install zsh-autosuggestions
-echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+echo "source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh" >>~/.zshrc
 
 brew install starship
-echo 'eval "\$(starship init zsh)"' >> ~/.zshrc
+echo 'eval "\$(starship init zsh)"' >>~/.zshrc
 
 brew tap michel-kraemer/zsh-patina && brew install zsh-patina
-echo 'eval "\$($(brew --prefix)/bin/zsh-patina activate)"' >> ~/.zshrc
+echo 'eval "\$($(brew --prefix)/bin/zsh-patina activate)"' >>~/.zshrc
 
 brew install fzf
-echo 'source <(fzf --zsh)' >> ~/.zshrc
+echo 'source <(fzf --zsh)' >>~/.zshrc
 
+brew install maccy
